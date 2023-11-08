55a21869d117e62dadb2d1f09e6fbc13687dea8c
pranjan03 <prashant.ranjan@nagarro.com>
2023-11-08 15:25:29 +0530

    Update SpringdemoApplication.java

--- src/main/java/com/nagarro/demo/SpringdemoApplication.java
+++ src/main/java/com/nagarro/demo/SpringdemoApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringdemoApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringdemoApplication.class, args);
+		System.out.print("Hi");
 	}
 
 }
